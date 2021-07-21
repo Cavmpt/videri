33165fad4ed5726602bfbbbd88754db59d174dde
cavmpt <keviny4n@gmail.com>
2021-07-21 08:46:52 -0400

    adding daily calendar feature

--- src/Components/UIComponents/WeatherCalendar/WeeklyCalendar/DailyWeatherSlice/DailyWeatherSlice.tsx
+++ src/Components/UIComponents/WeatherCalendar/WeeklyCalendar/DailyWeatherSlice/DailyWeatherSlice.tsx
@@ -2,15 +2,25 @@
 import React, {useContext} from 'react'
 import {Context, ContextType} from '../../../../../Store/Provider'
 
+import {IDateTimeInterface} from '../../../../../Helpers/dt-to-datetime'
+
 import './DailyWeatherSlice.scss'
 
 export interface IDailyWeatherSliceProps {
-  
+  date: IDateTimeInterface
+  highTemp: number
+  lowTemp: number
+  pop: number
+  icon: string
 }
 
 export default function DailyWeatherSlice(props: IDailyWeatherSliceProps) {
-  const context = useContext<ContextType>(Context)
-  const {dailyWeather} = context
+  const {date, highTemp, lowTemp, pop, icon} = props
 
-  return <div className='weekly-calendar-slice'></div>
+  return (
+    <div className='weekly-calendar-slice'>
+      {date}
+      {highTemp} {lowTemp} {pop} {icon}
+    </div>
+  )
 }
--- src/Components/UIComponents/WeatherCalendar/WeeklyCalendar/WeeklyCalendar.tsx
+++ src/Components/UIComponents/WeatherCalendar/WeeklyCalendar/WeeklyCalendar.tsx
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import React, {useContext} from 'react'
+import DailyWeatherSlice from './DailyWeatherSlice/DailyWeatherSlice'
 import {Context, ContextType} from '../../../../Store/Provider'
+
+import {unixTimestampToDate} from '../../../../Helpers/dt-to-datetime'
+import RectangularSkeleton from '../../Skeleton/RectangularSkeleton/RectangularSkeleton'
 
 import './WeeklyCalendar.scss'
 
@@ -12,7 +16,50 @@ export default function WeeklyCalendar(
   props: IWeeklyCalendarProps,
 ): JSX.Element {
   const context = useContext<ContextType>(Context)
-  const dailyTemperature = context
+  const {dailyWeather} = context
 
-  return <div>fff</div>
+  const CalendarPannel = () => {
+    if (dailyWeather !== undefined) {
+      return (
+        <>
+          {dailyWeather.map(item => {
+            const date = unixTimestampToDate(item.dt)
+            const highTemp = item.temp.max
+            const lowTemp = item.temp.min
+            const tempratureIcon = item.weather[0].icon
+            const {pop} = item
+            return (
+              <DailyWeatherSlice
+                date={date}
+                highTemp={highTemp}
+                lowTemp={lowTemp}
+                icon={tempratureIcon}
+                pop={pop}
+              />
+            )
+          })}
+        </>
+      )
+    }
+    if (dailyWeather === undefined) {
+      return (
+        <>
+          <RectangularSkeleton />
+          <RectangularSkeleton />
+          <RectangularSkeleton />
+          <RectangularSkeleton />
+          <RectangularSkeleton />
+          <RectangularSkeleton />
+          <RectangularSkeleton />
+        </>
+      )
+    }
+    return <div>No Daily Temperature Availble</div>
+  }
+
+  return (
+    <div>
+      <CalendarPannel />
+    </div>
+  )
 }
